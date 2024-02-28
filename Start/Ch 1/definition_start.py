b0339f38d0a994f804a8030d83de20553583367c
anudsa <133996511+anudsa@users.noreply.github.com>
2024-02-28 00:17:29 +0000

    init

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -1,11 +1,13 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Basic class definitions
-
-
-# TODO: create a basic class
-
-
-# TODO: create instances of the class
-
-
+# TODO: create a basic class that represents a book
+class Book:
+  def __init__(self,title):
+    self.title = title
+# TODO: create instances of the class // call the class name as a constructor function
+libro1 = Book("1984")
+libro2 = Book("un mundo feliz")
 # TODO: print the class and property
+print(libro1)
+print(libro1.title)
+print(libro2.title)
--- Start/Ch 1/instance_start.py
+++ Start/Ch 1/instance_start.py
@@ -1,25 +1,38 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using instance methods and attributes
-
-
 class Book:
     # the "init" function is called when the instance is
     # created and ready to be initialized
-    def __init__(self, title):
+    def __init__(self, title,pages,price):
         self.title = title
+        self.pages = pages
+        self.price = price
+        self.__secretNumberOfSales = 3.50
         # TODO: add properties
-
+        self.getPrice()
     # TODO: create instance methods
-
-
+    def getPrice(self):
+        if hasattr(self,"_discount"):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price
+    def setDiscount(self,discountPercentage):
+        self._discount = discountPercentage
+    def getSecret(self):
+        return self.__secretNumberOfSales
 # TODO: create some book instances
-b1 = Book("War and Peace")
-b2 = Book("The Catcher in the Rye")
+libro1 = Book("1984",120,9.99)
+libro2 = Book("un mundo feliz",100,5.99)
 
 # TODO: print the price of book1
-
-
+#print(libro1.price)
+print(libro1.getPrice())
+print(libro2.getPrice())
 # TODO: try setting the discount
-
-
+#discount is applied
+libro2.setDiscount(.5)
+print(libro2.getPrice())
+print(libro2._discount)
 # TODO: properties with double underscores are hidden by the interpreter
+print(libro1.getSecret())
+print(libro2._Book__secretNumberOfSales)
