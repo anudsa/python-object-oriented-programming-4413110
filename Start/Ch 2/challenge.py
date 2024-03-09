d2591465cca221b861ebe0252aee6482c7be6ee9
anudsa <133996511+anudsa@users.noreply.github.com>
2024-03-09 18:40:04 +0000

    Inheritance challenge completed

--- Start/Ch 2/challenge.py
+++ Start/Ch 2/challenge.py
@@ -1,26 +1,44 @@
-# Python Object Oriented Programming by Joe Marini course example
 # Programming challenge: use inheritance and abstract classes
-
 # Challenge: create a class structure to represent stocks and bonds
 # Requirements:
 # -- Both stocks and bonds have a price
 # -- Stocks have a company name and ticker
 # -- Bonds have a description, duration, and yield
 # -- You should not be able to instantiate the base class
-# -- Subclasses are required to override get_description()
-# -- get_description returns formats for stocks and bonds
+# -- Subclasses are required to override getDescription()
+# -- getDescription returns formats for stocks and bonds
 # For stocks: "Ticker: Company -- $Price"
 # For bonds: "description: duration'yr' : $price : yieldamt%"
+#Since both stocks and bonds are assets, this one is the superclass.
+from abc import ABC, abstractmethod
 
-class Asset():
-    pass
+class Asset(ABC):
+    def __init__(self,price):
+        #We initialize the super class (abc)
+        super().__init__()
+        self.price=price
 
-class Stock():
-    pass
+    @abstractmethod
+    def getDescription():
+        pass
 
-class Bond():
-    pass
 
+class Stock(Asset):
+    def __init__(self,ticker,price,company):
+        super().__init__(price)
+        self.company=company
+        self.ticker=ticker
+    def getDescription(self):
+        return f"{self.ticker}: {self.company} -- {self.price}"
+
+class Bond(Asset):
+    def __init__(self, price,description,duration,yieldPercentage):
+        super().__init__(price)
+        self.description=description
+        self.duration=duration
+        self.yieldPercentage=yieldPercentage
+    def getDescription(self):
+        return f"{self.description}: {self.duration} yr: ${self.price}: {self.yieldPercentage}%"
 
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
 try:
@@ -38,12 +56,12 @@ us10yr = Bond(96.70, "10 Year US Treasury", 10, 4.28)
 us5yr = Bond(98.65, "5 Year US Treasury", 5, 4.43)
 us2yr = Bond(99.57, "2 Year US Treasury", 2, 4.98)
 
-print(msft.get_description())
-print(goog.get_description())
-print(meta.get_description())
-print(amzn.get_description())
+print(msft.getDescription())
+print(goog.getDescription())
+print(meta.getDescription())
+print(amzn.getDescription())
 
-print(us30yr.get_description())
-print(us10yr.get_description())
-print(us5yr.get_description())
-print(us2yr.get_description())
+print(us30yr.getDescription())
+print(us10yr.getDescription())
+print(us5yr.getDescription())
+print(us2yr.getDescription())
